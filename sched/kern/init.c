d53e34fbde9a787a27592aa99dd99886cd027477
gabrieldiem <gabriel.diem5@gmail.com>
2024-10-17 22:37:50 -0300

    fix: fixed typo

--- sched/kern/init.c
+++ sched/kern/init.c
@@ -80,7 +80,7 @@ i386_init(void)
 	env_run(&envs[0]);
 
 	// Schedule and run the first user environment!
-	//sched_yield();
+	// sched_yield();
 }
 
 // While boot_aps is booting a given CPU, it communicates the per-core
